f07bd27b019396927a067212ad0d11ae7f03f3cb
Kapya <example@example.com>
2025-09-20 13:39:19 +0200

    removed unused col

--- treasury_bill_config_data_prep.R
+++ treasury_bill_config_data_prep.R
@@ -3,7 +3,6 @@ treasury_bill_approx_interest_rates <- c(0.11, 0.12, 0.13, 0.14)
 
 treasury_bill_config <- data.frame(
   tenure = treasury_bill_tenures_days,
-  investment_type = rep("Treasury Bill"),
   interest_rate = treasury_bill_approx_interest_rates,
   tax_rate = rep(0.15),
   handling_fee = rep(0.01)
