799e5284da561f80c0a276f5f096fc149d6090a7
Kapya <example@example.com>
2025-09-20 13:41:10 +0200

    scrape zm bonds data and prep columns

---
+++ gvt_bond_data_prep.R
@@ -0,0 +1,15 @@
+# Prepare zm bond interest data
+zm_gb_interest_rates <- zm_bonds_scrape()
+
+zm_gb_interest_rates <- zm_gb_interest_rates %>%
+  # Get only rows that have maturity period greater than a year
+  filter(str_detect(residual_maturity, "years")) %>%
+  mutate(
+    # Get the number of years from residual maturity string
+    maturity_years = str_extract(residual_maturity, one_or_more(DIGIT)) %>%
+      as.numeric(),
+    # Get number of coupon payments : 2 per year
+    num_coupon_payments = maturity_years * 2,
+    tax_rate = rep(0.15),
+    handling_fee = rep(0.01)
+  )
